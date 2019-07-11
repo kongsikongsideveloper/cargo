826f73756af498f4e75e2046ce3f7d50e8da4ba1
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-07-11 21:31:06 +0700

    commit

--- php/check-user-exists-by-phone.php
+++ php/check-user-exists-by-phone.php
@@ -1,7 +1,7 @@
 <?php
 include 'db.php';
 $phone = $_POST["phone"];
-$results = $c->query("SELECT * FROM users WHERE phone LIKE '%" . $phone . "%'");
+$results = $c->query("SELECT * FROM users WHERE phone='" . $phone . "'");
 if ($results && $results->num_rows > 0) {
 	echo 0;
 } else {
