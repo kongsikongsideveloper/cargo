b92afb0cd8e868303931a506f9df4f846f7989b4
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-07-12 06:18:27 +0700

    commit

--- php/db.php
+++ php/db.php
@@ -1,6 +1,3 @@
 <?php
 $c = new mysqli("localhost", "kongsi_admin", "HelloWorld123");
 $c->select_db("kongsi_cargo");
-if ($c->connect_error) {
-   echo("Connection failed: " . $c->connect_error);
-
